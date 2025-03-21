5468b067e06798533084ef1f52572ca8265ab59d
Raoul NIYO <vincenira@gmail.com>
2025-03-20 23:59:57 -0400

    initial test

--- fun1/jambo.go
+++ fun1/jambo.go
@@ -1,5 +1,18 @@
 package main
 
+import "fmt"
+
 /*
 Here we separated the domain "code" from the side effect
+which is the print of the word jambo wetu
+because the print and instantiation for the word allows
+an easy refactoring and separated concern
 */
+
+func Jambo() string {
+	return "Jambo wetu"
+}
+
+func main() {
+	fmt.Println(Jambo())
+}
