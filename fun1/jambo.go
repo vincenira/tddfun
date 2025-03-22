faa9b9704cc448d634da446162d4ca5816c8ac82
Raoul NIYO <vincenira@gmail.com>
2025-03-22 01:23:29 -0400

    add multiple language greetings with a working test

--- fun1/jambo.go
+++ fun1/jambo.go
@@ -11,12 +11,42 @@ an easy refactoring and separated concern
 
 // Const information on the variable helps to capture the essential of the variable and also aid the performance of the program
 // the essential, here is that the variable will not change throughout the execution life of the program
-const greetingWithPrefix = "Jambo, "
+const (
+	swahiliGreetingWithPrefix = "Jambo, "
+	englishGreetingWithPrefix = "Hello, "
+	spanishGreetingWithPrefix = "Hola, "
+	frenchGreetingWithPrefix  = "Salut, "
+	chineseGreetingWithPrefix = "Nǐ hǎo, "
+)
 
-func Jambo(Name string) string {
-	return greetingWithPrefix + Name
+func Jambo(Name, Language string) string {
+	response := ""
+	if Name == "" || Language == "" {
+		response = swahiliGreetingWithPrefix + "wetu"
+	}
+
+	if Language == "Swahili" {
+		response = swahiliGreetingWithPrefix + Name
+	}
+
+	if Language == "English" {
+		response = englishGreetingWithPrefix + Name
+	}
+
+	if Language == "Spanish" {
+		response = spanishGreetingWithPrefix + Name
+	}
+
+	if Language == "French" {
+		response = frenchGreetingWithPrefix + Name
+	}
+
+	if Language == "Chinese" {
+		response = chineseGreetingWithPrefix + Name
+	}
+	return response
 }
 
 func main() {
-	fmt.Println(Jambo("Peter"))
+	fmt.Println(Jambo("Peter", "english"))
 }
--- fun1/jambo_test.go
+++ fun1/jambo_test.go
@@ -6,15 +6,22 @@ func TestJambo(t *testing.T) {
 	// %q placeholder wrap your value into double quotes
 	// Reference from Go in fmt printing symbol https://pkg.go.dev/fmt#hdr-Printing
 	t.Run("Default: Greetings in swahili", func(t *testing.T) {
-		got := Jambo("")
+		got := Jambo("", "")
 		want := "Jambo, wetu"
 		if got != want {
 			t.Errorf("got %q want %q", got, want)
 		}
 	})
 
+	t.Run("Swahili: Greetings in swahili", func(t *testing.T) {
+		got := Jambo("wetu", "Swahili")
+		want := "Jambo, wetu"
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
 	t.Run("English: Greeting", func(t *testing.T) {
-		got := Jambo("wetu")
+		got := Jambo("wetu", "English")
 		want := "Hello, wetu"
 		if got != want {
 			t.Errorf("got %q want %q", got, want)
@@ -22,7 +29,7 @@ func TestJambo(t *testing.T) {
 	})
 
 	t.Run("Spanish: Greetings", func(t *testing.T) {
-		got := Jambo("wetu")
+		got := Jambo("wetu", "Spanish")
 		want := "Hola, wetu"
 		if got != want {
 			t.Errorf("got %q want %q", got, want)
@@ -30,7 +37,7 @@ func TestJambo(t *testing.T) {
 	})
 
 	t.Run("French: Greetings", func(t *testing.T) {
-		got := Jambo("wetu")
+		got := Jambo("wetu", "French")
 		want := "Salut, wetu"
 		if got != want {
 			t.Errorf("got %q want %q", got, want)
@@ -38,8 +45,8 @@ func TestJambo(t *testing.T) {
 	})
 
 	t.Run("Chinese: Greetings", func(t *testing.T) {
-		got := Jambo("wetu")
-		want := "Nǐ hǎo, Wetu"
+		got := Jambo("wetu", "Chinese")
+		want := "Nǐ hǎo, wetu"
 		if got != want {
 			t.Errorf("got %q want %q", got, want)
 		}
