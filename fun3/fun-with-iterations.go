73ed693a182ab6f600e811f9a950f38fa76b8e0f
Raoul NIYO <vincenira@gmail.com>
2025-03-23 17:15:58 -0400

    refactor: add variable repeatedCount and benchmark function
    add variable repeatedcount to facilitate the reusability of code
    add the benchmark function to learn how it works

--- fun3/fun-with-iterations.go
+++ fun3/fun-with-iterations.go
@@ -1,8 +1,37 @@
 package fun3
 
+/*
+Write the minimal amout of code for the test to run and check the failing test output
+All you need to go right now is enough to make it compile so you can check your test is written well.
+
+For example: this example is enough to compile and verify it your testing case works.
+```go
+package iteration
+
+	func Repeat(character string) string{
+	   return ""
+	}
+
+```
+*/
+/*
+Refactor1: create a variable for the number of times to repeat a character
+					 since it won't change in throughout lifetime execution of the program
+					 we will use a constant
+*/
+const repeatedCount = 7
+
+/*
+In golang, strings are immutable therefore everytime we append a character, a new memory slot
+is created, the process to clean up the old one, it tiggers at the same time.
+Hence, the code is not optimize performance wise.
+Go provides us with the method strings.Builder of type stringsBuilder which minimize memory
+copiying and to get the string you use the method String
+*/
 func Repeat(letter string) string {
 	retLetter := ""
-	for range 7 {
+	for range repeatedCount {
+		// += called the Add AND assignment operator""
 		retLetter += letter
 	}
 	return retLetter
--- fun3/fun-with-iterations_test.go
+++ fun3/fun-with-iterations_test.go
@@ -9,3 +9,18 @@ func TestRepeat(t *testing.T) {
 		t.Errorf("expected %q but got %q", expected, repeated)
 	}
 }
+
+/*
+the benchmark feature is provided by the standard library in Golang
+Benchmark library manages for you, the number of times to run your code
+through the variable b.N
+And to run the benchmark function "go test -bench=."
+Note:
+- that the benchmark and testing functions can coexist in the same _test.go file
+- By default benchmarks are run sequentially
+*/
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a")
+	}
+}
