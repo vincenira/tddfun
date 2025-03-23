8393ef60411f3572dbda279f3229df08b5cfbadf
Raoul NIYO <vincenira@gmail.com>
2025-03-22 22:22:26 -0400

    Add working version of the function
    add working version of the function Adder
    add an example of a good comments

--- fun2/fun-with-integers.go
+++ fun2/fun-with-integers.go
@@ -2,6 +2,14 @@ package fun2
 
 // Initial function with minimal code to make the go compiler not fail
 // to see if the test works as intended and then will refactor
+
+/*
+Concept of Property Based Testing and TDD workflow
+*/
+
+/*
+Example of a good comment: Add takes two arguments of type integers and returns the sum of them
+*/
 func Adder(x, y int) int {
-	return 6
+	return x + y
 }
