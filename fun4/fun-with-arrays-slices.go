6dc576a8568532ed183940f8d8975bc93f6e2cc1
Raoul NIYO <vincenira@gmail.com>
2025-03-26 22:35:29 -0400

    feat: End of the fun4
    played with arrays, slices, make, append, len
    played with coverage test and reflect.DeepEqual method

--- fun4/fun-with-arrays-slices.go
+++ fun4/fun-with-arrays-slices.go
@@ -1,5 +1,53 @@
 package fun4
 
 func Sum(numbers [5]int) int {
-	return 0
+	sum := 0
+	/*
+	   for i := 0; i < 5; i++ {
+	   refactor using the for range
+	*/
+	for _, number := range numbers {
+		sum += number
+	}
+	return sum
+}
+
+func SumSlice(numbers []int) int {
+	sum := 0
+	for _, number := range numbers {
+		sum += number
+	}
+	return sum
+}
+
+/*
+e.g: make([]int, 0, 5) creates a slice with length 0 and capacity 5.
+*/
+func SumAll(sliceNumbers ...[]int) []int {
+	var sumSlice []int
+	var sum int
+	for _, sliceNumber := range sliceNumbers {
+		sum = 0
+		for _, number := range sliceNumber {
+			sum += number
+		}
+		sumSlice = append(sumSlice, sum)
+	}
+	return sumSlice
+}
+
+func SumAllTails(sliceNumbers ...[]int) []int {
+	var sums []int
+	var sum int
+	for _, sliceNumber := range sliceNumbers {
+		sum = 0
+		for i, number := range sliceNumber {
+			if i == 0 {
+				continue
+			}
+			sum += number
+		}
+		sums = append(sums, sum)
+	}
+	return sums
 }
--- fun4/fun-with-arrays-slices_test.go
+++ fun4/fun-with-arrays-slices_test.go
@@ -5,7 +5,10 @@ hence Go will not allow you to import a package with name main
 */
 package fun4
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 	numbers := [5]int{1, 2, 3, 4, 5}
@@ -16,4 +19,61 @@ func TestSum(t *testing.T) {
 	if got != want {
 		t.Errorf("got %d want %d given, %v", got, want, numbers)
 	}
+}
+
+func TestSumSlice(t *testing.T) {
+	t.Run("Collection of 5 numbers", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
+		got := SumSlice(numbers)
+		want := 15
+		if got != want {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
+
+	t.Run("Collection of any numbers", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+		got := SumSlice(numbers)
+		want := 6
+		if got != want {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
+}
+
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+	// you can compare a slice using the equality sign we need the reflect.DeepEqual
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
+
+/*
+refactor the repeated parts of the testing code with a checksum function helper
+*/
+func TestSumAllTails(t *testing.T) {
+	/*
+		It's not shown here, but this technique can be useful when you want to bind a function to other
+		local variables in "scope" (e.g between some {}). It also allows you to reduce the surface area of your API.
+	*/
+	checksum := func(t testing.TB, got []int, want []int) {
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	}
+	t.Run("Make the sums of some integer slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+		// you can compare a slice using the equality sign we need the reflect.DeepEqual
+		checksum(t, got, want)
+	})
+	t.Run("Make the sums of an empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{3, 4, 5})
+		want := []int{0, 9}
+
+		checksum(t, got, want)
+	})
 }
