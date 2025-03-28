11446c2f2d86aad41edf2ff12193ef806334cbe2
Raoul NIYO <vincenira@gmail.com>
2025-03-27 22:37:41 -0400

    fix: add the function to calculate the Area

--- fun5/shapes.go
+++ fun5/shapes.go
@@ -13,3 +13,7 @@ func Perimeter(x, y float64) float64 {
 func Perimeter(width, height float64) float64 {
 	return 2 * (width + height)
 }
+
+func Area(width, height float64) float64 {
+	return width * height
+}
