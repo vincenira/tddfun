b8bdd647ca0eb8152dbaf17e06dcf8cc63aefb1b
Raoul NIYO <vincenira@gmail.com>
2025-03-27 22:24:34 -0400

    feat: add fun5
    add a test case
    add minimal amount of code for test to run

---
+++ fun5/go.mod
@@ -0,0 +1,3 @@
+module fun5
+
+go 1.23.1
---
+++ fun5/shapes.go
@@ -0,0 +1,8 @@
+package fun5
+
+// initial commit with test case and the minimal amount of code for test to run
+// and check the failing test output
+
+func Perimeter(x, y float64) float64 {
+	return 0.0
+}
