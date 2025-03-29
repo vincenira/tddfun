c62024698fb752d9b5db74256d05400eb0b493d1
Raoul NIYO <vincenira@gmail.com>
2025-03-28 20:02:53 -0400

    implement a test case for methods for rectangle and circle

--- fun5/shapes_test.go
+++ fun5/shapes_test.go
@@ -38,10 +38,35 @@ func TestPerimeter(t *testing.T) {
 
 func TestArea(t *testing.T) {
 	rectangle := Rectangle{10.0, 10.0}
-	got := Area(rectangle.width, rectangle.height)
+	// got := Area(rectangle.width, rectangle.height)
+	// refactor to pass the object Rectangle as an argument
+	got := Area(rectangle)
 	want := 100.0
 
 	if got != want {
 		t.Errorf("got %.2f want %.2f", got, want)
 	}
+}
+
+// Adding the circle calculation
+
+func TestGeometryShapes(t *testing.T) {
+	t.Run("Calculation the Area of Rectangle", func(t *testing.T) {
+		rectangle := Rectangle{10.0, 10.0}
+		got := rectangle.Area()
+		want := 100.0
+		if got != want {
+			t.Errorf("got %.2f want %.2f", got, want)
+		}
+	})
+
+	t.Run("Calculation the Area of Circle", func(t *testing.T) {
+		circle := Circle{10.0}
+		got := circle.Area()
+		want := 314.1592653589793
+		// f format string was replaced by g which is more precise
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
+	})
 }
