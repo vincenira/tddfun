84bb8ff28e7e01a03bee18bf130482012fc7526a
Raoul NIYO <vincenira@gmail.com>
2025-03-29 20:38:33 -0400

    refactor: add better logic for test and reduce duplication

--- fun5/shapes_test.go
+++ fun5/shapes_test.go
@@ -94,3 +94,71 @@ func TestShapeArea(t *testing.T) {
 		checkArea(t, circle, 314.1592653589793)
 	})
 }
+
+/*
+Learning the table driven tests
+
+table driven tests are used when you want to make all list of test cases which can be tested
+in the same manner
+*/
+
+func TestTDTArea(t *testing.T) {
+	// Anonymous struct which is a slice of structs by using []struct with two fields, the shape and the want
+	AreaTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		// composite literal declaration when need a comma for the last element
+		{Rectangle{12, 6}, 72.0},
+		{Circle{10}, 314.1592653589793},
+		// Add a test for the new shape the triangle
+		{Triangle{12, 6}, 36.0},
+	}
+
+	for _, tt := range AreaTests {
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("got %g want %g", got, tt.want)
+		}
+	}
+}
+
+// The above struct needs some refactor because it is not immediately clear
+// what all the numbers represent and we should be aiming for your tests to be
+// easily understood
+// In Test-Driven Development by Example Kent Beck refactors some tests to a point and asserts:
+//    "The test speaks to us more clearly, as if it were an assertion of truth, not a
+//     sequence of operations"
+// Now our tests - Rather, the list of test cases - make assertions of truth about shapes
+// and their areas.
+/*
+The below test function is a refacted code of the TestTDTArea to solve the following problems:
+- To assert the truth, not to have a sequence of operations
+- Allow the developer to know rapidly, the type of its fields when it fails
+- Increase readability of the test cases
+- By wrapping the testcases into t.Run we can run specify test
+	you can run specific tests within your table with go test -run TestTDTRefactedArea/Rectangle
+*/
+
+func TestTDTRefactedArea(t *testing.T) {
+	AreaTests := []struct {
+		name    string
+		shape   Shape
+		hasArea float64
+	}{
+		{name: "Rectangletoo", shape: Rectangle{Width: 12, Height: 6}, hasArea: 72.0},
+		{name: "Circle", shape: Circle{Radius: 10}, hasArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{Base: 12, Height: 6}, hasArea: 36.0},
+	}
+
+	for _, tt := range AreaTests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.hasArea {
+				// %#v format stirng will print out our struct with the values in its fields
+				// so the developer can see at glance the properties that are being tested
+				t.Errorf("%#v got %g want %g", tt.shape, got, tt.hasArea)
+			}
+		})
+	}
+}
