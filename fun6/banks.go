167bdf3bbfba54965dde65111e17ca31efe83d24
Raoul NIYO <vincenira@gmail.com>
2025-03-31 21:59:23 -0400

    fix: use pointers to reference the address, not to use the copy of balance

--- fun6/banks.go
+++ fun6/banks.go
@@ -10,6 +10,7 @@ type Wallet struct {
 func (w Wallet) Deposit(amount int)
 In go when you call a function or a method the arguments are copied
 */
+/*
 func (w Wallet) Deposit(amount int) {
 	// print the address of balance in the method
 	fmt.Printf("address of balance in Deposit is %p \n", &w.balance)
@@ -17,5 +18,15 @@ func (w Wallet) Deposit(amount int) {
 }
 
 func (w Wallet) Balance() int {
+	return w.balance
+}
+*/
+func (w *Wallet) Deposit(amount int) {
+	// print the address of balance in the method
+	fmt.Printf("address of balance in Deposit is %p \n", &w.balance)
+	w.balance += amount
+}
+
+func (w *Wallet) Balance() int {
 	return w.balance
 }
