dfc699d09fa3d9791bbd330ea00f7ae1edf31d56
Raoul NIYO <vincenira@gmail.com>
2025-03-31 22:24:18 -0400

    feat: add the withdraw function with its test

--- fun6/banks_test.go
+++ fun6/banks_test.go
@@ -6,17 +6,34 @@ import (
 )
 
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
 
-	wallet.Deposit(Bitcoin(10))
+		wallet.Deposit(Bitcoin(10))
 
-	got := wallet.Balance()
-	// print the address of the memory
-	fmt.Printf("address of balance in test is %p \n", &wallet.balance)
-	want := Bitcoin(20)
+		got := wallet.Balance()
+		// print the address of the memory
+		fmt.Printf("address of balance in test is %p \n", &wallet.balance)
+		want := Bitcoin(10)
 
-	// Update our test format to use strings
-	if got != want {
-		t.Errorf("got %s want %s", got, want)
-	}
+		// Update our test format to use strings
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	})
+
+	t.Run("Withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+
+		wallet.Withdraw(Bitcoin(10))
+
+		got := wallet.Balance()
+		// print the address of the memory
+		want := Bitcoin(10)
+
+		// Update our test format to use strings
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	})
 }
