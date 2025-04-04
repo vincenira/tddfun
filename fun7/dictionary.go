7740d0f808a55870f53109ac533ae301b3fc3d01
Raoul NIYO <vincenira@gmail.com>
2025-04-03 22:44:10 -0400

    refactor: make dictionnary a type

--- fun7/dictionary.go
+++ fun7/dictionary.go
@@ -1,5 +1,23 @@
 package fun7
 
-func Search(dict map[string]string, word string) string {
-	return dict[word]
+import (
+	"errors"
+)
+
+type Dictionary map[string]string
+
+func (d Dictionary) Search(word string) (string, error) {
+	definition, ok := d[word]
+
+	/*
+	   refactor: from a simple function to a method
+	   func Search(dict map[string]string, word string) string {
+	   	return dict[word]
+	   }
+	*/
+	// ok is a boolean which indicates if the key was found successfully or not
+	if !ok {
+		return "", errors.New("Could not find the word you were looking for")
+	}
+	return definition, nil
 }
--- fun7/dictionary_test.go
+++ fun7/dictionary_test.go
@@ -3,11 +3,32 @@ package fun7
 import "testing"
 
 func TestSearch(t *testing.T) {
-	dictionary := map[string]string{"test": "This is just a test"}
+	/*
+	   	dictionary := map[string]string{"test": "This is just a test"}
+	     Refactor: dictionnary to have search as a method of dictionnary whereas dictionnary
+	     is a type.
+	*/
+	dictionary := Dictionary{"test": "This is just a test"}
 
-	got := Search(dictionary, "test")
+	got, _ := dictionary.Search("test")
 	want := "This is just a test"
 	assertStrings(t, got, want)
+	/*
+	   Implementing a logic when the word is not in the dictionary
+	*/
+	t.Run("Know word", func(t *testing.T) {
+		got, _ := dictionary.Search("test")
+		want := "This is just a test"
+		assertStrings(t, got, want)
+	})
+	t.Run("Unknown word", func(t *testing.T) {
+		_, err := dictionary.Search("unknown")
+		want := "Could not find the word you were looking for"
+		if err == nil {
+			t.Fatal("Expected to get an error")
+		}
+		assertStrings(t, err.Error(), want)
+	})
 }
 
 func assertStrings(t testing.TB, got, want string) {
