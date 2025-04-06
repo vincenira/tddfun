25ed3a32e838e019c3a91ef896d0ed8eceb38b4b
Raoul NIYO <vincenira@gmail.com>
2025-04-06 15:46:58 -0400

    feat: add httphandler to send the musalimie to a different output media

--- fun8/dependency_injection_jambo.go
+++ fun8/dependency_injection_jambo.go
@@ -3,8 +3,23 @@ package fun8
 import (
 	"fmt"
 	"io"
+	"net/http"
 )
 
 func Musalimie(writer io.Writer, jina string) {
 	fmt.Fprintf(writer, "Jambo, %s", jina)
 }
+
+// Where else we can send information to print to a different media
+
+// Handler handles the response to the request to Musalimie
+func MusalimieHandler(w http.ResponseWriter, r http.Request) {
+	Musalimie(w, "dunia")
+}
+
+/*
+func main(){
+	 Musalimie(os.Stdout, "Mufasa")
+   log.Fatal(http.ListenAndServe(":5001", http.HnadlerFunc(MusalimieHandler)))
+}
+*/
