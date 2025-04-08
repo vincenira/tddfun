80e3abe2d63eb3bdc6243805663bef5600a45228
Raoul NIYO <vincenira@gmail.com>
2025-04-07 22:17:33 -0400

    feat: add the function countdown and its testcase

---
+++ fun9/mock_it_test.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"bytes"
+	"testing"
+)
+
+/*
+We know we want our Countdown function to write data somewhere and io.Writer is the de-facto
+way of capturing that as an interface in Go.
+- In main we will send to os.Stdout so our users see the countdown printed to the terminal.
+- In test we will send to bytes.Buffer so our tests can capture what data is being generated.
+*/
+func TestCountdown(t *testing.T) {
+	buffer := &bytes.Buffer{}
+
+	Countdown(buffer)
+
+	got := buffer.String()
+	want := "3"
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
