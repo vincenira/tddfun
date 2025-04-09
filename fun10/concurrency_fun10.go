e89a55f2d585fa06141afa62af4154ed3e3351bc
Raoul NIYO <vincenira@gmail.com>
2025-04-09 19:23:14 -0400

    feat: add concurrency experiment

---
+++ fun10/concurrency_fun10.go
@@ -0,0 +1,12 @@
+package fun10
+
+type WebsiteChecker func(string) bool
+
+func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
+	results := make([string]bool)
+
+	for _, url := range urls {
+		results[url] = wc(url)
+	}
+	return results
+}
