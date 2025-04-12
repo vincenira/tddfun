23943fa694924e4272acbe458e631129f63db68b
Raoul NIYO <vincenira@gmail.com>
2025-04-12 00:20:44 -0400

    feat: add the minimal amount of code for the test to run
    implement a simple Racer function

---
+++ fun11/select_race_it.go
@@ -0,0 +1,10 @@
+/*
+make a program that races two websites by hitting with HTTP GET
+and returning the URL which return first. if none of them return
+with 10 seconds then it should return an error
+*/
+package fun11
+
+func Racer(a, b string) (winner string) {
+	return
+}
