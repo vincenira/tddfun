b89c744dcf60145c25c84b35d98548c88f750c75
Raoul NIYO <vincenira@gmail.com>
2025-04-14 22:17:13 -0400

    chore: Document information about the tools used.

--- fun11/select_race_it.go
+++ fun11/select_race_it.go
@@ -62,3 +62,15 @@ func ConfigurableConcRacer(a, b string, timeout time.Duration) (winner string, e
 		return "", fmt.Errorf("time out waiting for %s and %s", a, b)
 	}
 }
+
+/*
+important:
+select:
+- Helps you wait on multiple channels
+- Sometimes you'll want to include time.After in one of your cases to prevent your system
+  blocking forever
+httptest:
+- Convenient way of creating test servers so you can have reliable and controllable tests.
+- uses the same interfaces as the "real" net/http servers which is consistent and less for you
+to learn
+*/
