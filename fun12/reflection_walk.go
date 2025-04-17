cecb5235104146464d57b252e6f9b10db74a6e6d
Raoul NIYO <vincenira@gmail.com>
2025-04-16 21:11:03 -0400

    refactor: use a switch statement instead of if statement

--- fun12/reflection_walk.go
+++ fun12/reflection_walk.go
@@ -15,12 +15,12 @@ type Profile struct {
 func walk(x interface{}, fn func(input string)) {
 	val := reflect.ValueOf(x)
 	for i := 0; i < val.NumField(); i++ {
-		field := val.Field(i)
-		if field.Kind() == reflect.String {
-			fn(field.String())
-		}
 
-		if field.Kind() == reflect.Struct {
+		field := val.Field(i)
+		switch field.Kind() {
+		case reflect.String:
+			fn(field.String())
+		case reflect.Struct:
 			walk(field.Interface(), fn)
 		}
 	}
