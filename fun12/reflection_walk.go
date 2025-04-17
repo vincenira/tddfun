69b55343351587a9e68ce3c3faa6463ba8b11242
Raoul NIYO <vincenira@gmail.com>
2025-04-16 21:19:28 -0400

    feat: add getValue function to inspect the type received

--- fun12/reflection_walk.go
+++ fun12/reflection_walk.go
@@ -13,7 +13,7 @@ type Profile struct {
 }
 
 func walk(x interface{}, fn func(input string)) {
-	val := reflect.ValueOf(x)
+	val := getValue(x)
 	for i := 0; i < val.NumField(); i++ {
 
 		field := val.Field(i)
@@ -24,4 +24,14 @@ func walk(x interface{}, fn func(input string)) {
 			walk(field.Interface(), fn)
 		}
 	}
+}
+
+func getValue(x interface{}) reflect.Value {
+	val := reflect.ValueOf(x)
+
+	if val.Kind() == reflect.Pointer {
+		val = val.Elem()
+	}
+
+	return val
 }
