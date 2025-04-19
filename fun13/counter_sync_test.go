7fd6bf68b660ab5f921c32e8719deb8a1b8cdc8b
Raoul NIYO <vincenira@gmail.com>
2025-04-19 16:10:42 -0400

    feat: add a testcase for a concurrent increment

--- fun13/counter_sync_test.go
+++ fun13/counter_sync_test.go
@@ -14,12 +14,32 @@ func TestCounter(t *testing.T) {
 
 		assertCounter(t, counter, 3)
 	})
+	/*
+	   A WaitGroup waits for a collection of goroutines to finish. The main goroutine calls
+	   Add to set the number of goroutines to wait for. Then each of the goroutines runs and
+	   calls Done when finished. At the same time, Wait can be used to block until all goroutines
+	   have finished.
+	*/
+	t.Run("it runs safely concurrently", func(t *testing.T) {
+		wantedCount := 1000
+		counter := Counter{}
+		var wg sync.WaitGroup
+		wg.Add(wantedCount)
+		for i := 0; i < wantedCount; i++ {
+			go func() {
+				counter.Inc()
+				wg.Done()
+			}()
+		}
+		wg.Wait()
+		assertCounter(t, counter, wantedCount)
+	})
 }
 
 func assertCounter(t testing.TB, got Counter, want int) {
 	t.Helper()
 
 	if got.Value() != want {
-		t.Errorf("got %d, want %d", got.Value(), 3)
+		t.Errorf("got %d, want %d", got.Value(), want)
 	}
 }
