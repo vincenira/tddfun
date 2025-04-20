d7e24961bd23f3b702bc4be041c8f1568153171f
Raoul NIYO <vincenira@gmail.com>
2025-04-20 17:14:49 -0400

    feat: add fetch and cancel functionality

---
+++ fun14/storeContext_test.go
@@ -0,0 +1,36 @@
+package fun14
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
+
+type SpyStore struct {
+	response string
+	cancelled bool
+}
+
+func (s *SpyStore) Fetch() string {
+	time.Sleep(100 * time.Millisecond)
+	return s.response
+}
+
+func (s *SpyStore) Cancel(){
+	s.cancelled = true
+}
+
+func TestServer(t *testing.T) {
+	data := "hello, world"
+	svr := Server(&SpyStore{data})
+
+	request := httptest.NewRequest(http.MethodGet, "/", nil)
+	response := httptest.NewRecorder()
+
+	svr.ServeHTTP(response, request)
+
+	if response.Body.String() != data {
+		t.Errorf('got "%s", want "%s"', response.Body.String(), data)
+	}
+}
