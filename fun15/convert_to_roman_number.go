f980be22515f347c1a40436f24057886f621c438
Raoul NIYO <vincenira@gmail.com>
2025-05-03 08:15:42 -0400

    Refactor: add logic for the roman number

--- fun15/convert_to_roman_number.go
+++ fun15/convert_to_roman_number.go
@@ -1,23 +1,40 @@
 package fun15
 
+import "strings"
+
 /*
 It looks that we are not actually tackling the problem, so we need to write more tests
 to drive us forward
 check TestOld2RomanNumerals
-func ConvertToRoman(arabicNumber int) string {
-	if arabicNumber == 2 {
-		return "II"
+
+	func ConvertToRoman(arabicNumber int) string {
+		if arabicNumber == 2 {
+			return "II"
+		}
+		return "I"
 	}
-	return "I"
-}
 */
+type RomanNumeral struct {
+	Value  int
+	Symbol string
+}
+
+var allRomanNumerals = []RomanNumeral{
+	{10, "X"},
+	{9, "IX"},
+	{5, "V"},
+	{4, "IV"},
+	{1, "I"},
+}
 
 func ConvertToRoman(arabicNumber int) string {
-	if arabicNumber == 3 {
-		return "III"
+	var result strings.Builder
+
+	for _, numeral := range allRomanNumerals {
+		for arabicNumber >= numeral.Value {
+			result.WriteString(numeral.Symbol)
+			arabicNumber -= numeral.Value
+		}
 	}
-	if arabicNumber == 2 {
-		return "II"
-	}
-	return "I"
+	return result.String()
 }
