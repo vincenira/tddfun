faaba0b111f2b2b1882127f58ab65e0b945b8568
Raoul NIYO <vincenira@gmail.com>
2025-05-08 21:55:19 -0400

    chore: add a testcase for the clockface

---
+++ fun16/clockface_test.go
@@ -0,0 +1,18 @@
+package clockface_test
+
+import (
+	"projetpath/clockface"
+	"testing"
+	"time"
+)
+
+func TestSecondHandAtMidnight(t *testing.T) {
+	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
+
+	want := clockface.Point{X: 150, Y: 150 - 90}
+	got := clockface.SecondHand(tm)
+
+	if got != want {
+		t.Errorf("Got %v, want %v", got, want)
+	}
+}
