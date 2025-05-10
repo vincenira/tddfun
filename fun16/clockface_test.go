d98f73b014bc419208a77a068af730fa50139a96
Raoul NIYO <vincenira@gmail.com>
2025-05-10 18:56:44 -0400

    refactor: replace the package name with the current one
    remove the mypackage.function/method since test is within the package

--- fun16/clockface_test.go
+++ fun16/clockface_test.go
@@ -1,4 +1,4 @@
-package clockface_test
+package fun16
 
 import (
 	"testing"
@@ -8,8 +8,8 @@ import (
 func TestSecondHandAtMidnight(t *testing.T) {
 	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
 
-	want := clockface.Point{X: 150, Y: 150 - 90}
-	got := clockface.SecondHand(tm)
+	want := Point{X: 150, Y: 150 - 90}
+	got := SecondHand(tm)
 
 	if got != want {
 		t.Errorf("Got %v, want %v", got, want)
