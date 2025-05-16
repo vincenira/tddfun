27f4930d6b60ee5b72f56fe826fe57ce909c32ea
Raoul NIYO <vincenira@gmail.com>
2025-05-15 22:23:12 -0400

    refactor: add xml writer for svg instead of using pure string

--- fun16/clockface_test.go
+++ fun16/clockface_test.go
@@ -1,6 +1,8 @@
 package fun16
 
 import (
+	"bytes"
+	"encoding/xml"
 	"math"
 	"testing"
 	"time"
@@ -76,6 +78,24 @@ func TestSecondHandAt30Seconds(t *testing.T) {
 	if got != want {
 		t.Errorf("Got %v, wanted %v", got, want)
 	}
+}
+
+func TestSVGWriterAtMidnight(t *testing.T) {
+	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
+	b := bytes.Buffer{}
+	SVGWriter(&b, tm)
+	svg := Svg{}
+	xml.Unmarshal(b.Bytes(), &svg)
+
+	x2 := "150"
+	y2 := "60"
+
+	for _, line := range svg.Line {
+		if line.X2 == x2 && line.Y2 == y2 {
+			return
+		}
+	}
+	t.Errorf("Expected to find the second hand with x2 of %+v and y2 of %+v, in the SVG output %v", x2, y2, b.String())
 }
 
 func roughlyEqualFloat64(a, b float64) bool {
