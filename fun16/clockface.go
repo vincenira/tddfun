5ca3211bc45ad85981cb5b42100140ae15488315
Raoul NIYO <vincenira@gmail.com>
2025-05-15 22:22:36 -0400

    feat: add an xml data structure

--- fun16/clockface.go
+++ fun16/clockface.go
@@ -1,6 +1,7 @@
 package fun16
 
 import (
+	"encoding/xml"
 	"math"
 	"time"
 )
@@ -8,6 +9,31 @@ import (
 type Point struct {
 	X float64
 	Y float64
+}
+
+type Svg struct {
+	XMLName xml.Name `xml:"svg"`
+	Text    string   `xml:",chardata"`
+	Xmlns   string   `xml:"xmlns,attr"`
+	Width   string   `xml:"width,attr"`
+	Height  string   `xml:"height,attr"`
+	ViewBox string   `xml:"viewBox,attr"`
+	Version string   `xml:"version,attr"`
+	Circle  struct {
+		Text  string `xml:",chardata"`
+		Cx    string `xml:"cx,attr"`
+		Cy    string `xml:"cy,attr"`
+		R     string `xml:"r,attr"`
+		Style string `xml:"style,attr"`
+	} `xml:"circle"`
+	Line []struct {
+		Text  string `xml:",chardata"`
+		X1    string `xml:"x1,attr"`
+		Y1    string `xml:"y1,attr"`
+		X2    string `xml:"x2,attr"`
+		Y2    string `xml:"y2,attr"`
+		Style string `xml:"style,attr"`
+	} `xml:"line"`
 }
 
 // SecondHand is the unit vector of the second hand of an analogue clock at time 't'
