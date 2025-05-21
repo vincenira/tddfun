6ac1d921eb1a33658c3e70cbdaeb8e6bc62f5a0a
Raoul NIYO <vincenira@gmail.com>
2025-05-20 23:35:49 -0400

    refactor: redesign the testing for SVG minutes representation

--- fun16/clockface_test.go
+++ fun16/clockface_test.go
@@ -84,18 +84,53 @@ func TestSVGWriterAtMidnight(t *testing.T) {
 	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
 	b := bytes.Buffer{}
 	SVGWriter(&b, tm)
-	svg := Svg{}
+	svg := SVG{}
 	xml.Unmarshal(b.Bytes(), &svg)
 
-	x2 := "150"
-	y2 := "60"
+	want := Line{150, 150, 150, 60}
 
 	for _, line := range svg.Line {
-		if line.X2 == x2 && line.Y2 == y2 {
+		if line == want {
 			return
 		}
 	}
-	t.Errorf("Expected to find the second hand with x2 of %+v and y2 of %+v, in the SVG output %v", x2, y2, b.String())
+	t.Errorf("Expected to find the second hand line %+v, in the SVG lines %+v", want, svg.Line)
+}
+
+func TestSVGWriterSecondHand(t *testing.T) {
+	cases := []struct {
+		time time.Time
+		line Line
+	}{
+		{
+			simpleTime(0, 0, 0),
+			Line{150, 150, 150, 60},
+		}, {
+			simpleTime(0, 0, 30),
+			Line{150, 150, 150, 240},
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(testName(c.time), func(t *testing.T) {
+			b := bytes.Buffer{}
+			SVGWriter(&b, c.time)
+			svg := SVG{}
+			xml.Unmarshal(b.Bytes(), &svg)
+			if !containsLine(c.line, svg.Line) {
+				t.Errorf("Expected to find the second hand line %+v, in the SVG lines %+v", c.line, svg.Line)
+			}
+		})
+	}
+}
+
+func containsLine(l Line, ls []Line) bool {
+	for _, line := range ls {
+		if line == l {
+			return true
+		}
+	}
+	return false
 }
 
 func roughlyEqualFloat64(a, b float64) bool {
