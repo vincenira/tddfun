9c01cde82dc1682858b33d31eb4b96c39411e7b7
Raoul NIYO <vincenira@gmail.com>
2025-05-26 21:31:20 -0400

    test: add testing for minutes and hours

--- fun16/clockface_test.go
+++ fun16/clockface_test.go
@@ -188,6 +188,92 @@ func TestMinuteHandPoint(t *testing.T) {
 	}
 }
 
+func TestSVGWriterHourHand(t *testing.T){
+	cases := []struct{
+		time time.Time
+		line Line
+	}{
+		{
+			simpleTime(6, 0, 0),
+			Line{150, 150, 150, 200},
+		}
+	}
+	for _, c := range cases{
+		t.Run(testName(c.time), func(t *testing.T){
+			b := bytes.Buffer{}
+			SVGWriter(&b, c.time)
+			svg := SVG{}
+			xml.Unmarshal(b.Bytes(), &svg)
+			if !containsLine(c.line, svg.Line){
+				t.Errorf("Expected to find the hour hand line %+v, in the SVG line %+v", c.line, svg.Line)
+			}
+		})
+	}
+}
+
+func TestHoursInRadians(t *testing.T){
+	cases := []struct{
+		time time.Time
+		angle float64
+	}{
+		{simpleTime(6, 0, 0), math.Pi},
+		{simpleTime(0, 0, 0), 0},
+		{simpleTime(21, 0, 0), math.Pi * 1.5},
+		{simpleTime(0, 1, 30), math.Pi / ((6 * 60 * 60) / 90)},
+	}
+
+	for _, c := range cases{
+		t.Run(testName(c.time), func(t *testing.T){
+			got := HoursInRadians(c.time)
+			if !roughlyEqualFloat64(got, c.angle){
+				t.Fatalf("Wanted %v radians, but got %v", c.angle, got)
+			}
+		})
+	}
+}
+func TestHourHandPoint(t *testing.T) {
+	cases := []struct {
+		time  time.Time
+		point Point
+	}{
+		{simpleTime(6, 0, 0), Point{0, -1}},
+		{simpleTime(21, 0, 0), Point{-1, 0}},
+	}
+
+	for _, c := range cases {
+		t.Run(testName(c.time), func(t *testing.T) {
+			got := hourHandPoint(c.time)
+			if !roughlyEqualPoint(got, c.point) {
+				t.Fatalf("Wanted %v Point, but got %v", c.point, got)
+			}
+		})
+	}
+}
+func TestSVGWriterHourHand(t *testing.T) {
+	cases := []struct {
+		time time.Time
+		line Line
+	}{
+		{
+			simpleTime(6, 0, 0),
+			Line{150, 150, 150, 200},
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(testName(c.time), func(t *testing.T) {
+			b := bytes.Buffer{}
+			clockface.SVGWriter(&b, c.time)
+
+			svg := SVG{}
+			xml.Unmarshal(b.Bytes(), &svg)
+
+			if !containsLine(c.line, svg.Line) {
+				t.Errorf("Expected to find the hour hand line %+v, in the SVG lines %+v", c.line, svg.Line)
+			}
+		})
+	}
+}
 func containsLine(l Line, ls []Line) bool {
 	for _, line := range ls {
 		if line == l {
