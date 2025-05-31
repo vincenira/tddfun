7d739266eb2fcb48415a05edcbdd33ee4c448579
Raoul NIYO <vincenira@gmail.com>
2025-05-31 19:04:26 -0400

    refactor: add an first test

--- fun17/mypackage/blogposts_test.go
+++ fun17/mypackage/blogposts_test.go
@@ -26,9 +26,36 @@ func TestNewPosts(t *testing.T) {
 		"hello-world2.md": {Data: []byte("hola")},
 	}
 
-	posts := mypackage.NewPostsFromFS(fs)
+	/*
+	   To acknowledge that errors can happen when working with files. let's handle error
+	*/
+
+	posts, err := mypackage.NewPostsFromFS(fs)
+	if err != nil {
+		t.Fatal(err)
+	}
 
 	if len(posts) != len(fs) {
 		t.Errorf("got %d posts, wanted %d posts", len(posts), len(fs))
+	}
+}
+
+// Write the test first
+func TestNewBlogPosts(t *testing.T) {
+	fs := fstest.MapFS{
+		"hello world.md":  {Data: []byte("Title: Post 1")},
+		"hello-world2.md": {Data: []byte("Title: Post 2")},
+	}
+
+	posts, err := mypackage.NewPostsFromFS(fs)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	got := posts[0]
+	want := mypackage.Post{Title: "Post 1"}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %+v, want %+v", got, want)
 	}
 }
