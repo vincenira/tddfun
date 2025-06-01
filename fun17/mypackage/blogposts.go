927a7b849ea627b9d74fb766ad50f123fefac818
Raoul NIYO <vincenira@gmail.com>
2025-05-31 20:03:16 -0400

    refactor: add the progression of decoupling the GetPost function
    decouple the function by separating opening the file and extracting the
    data
    decouple the function resulted from the separating based on argument
    decouple from fs.FS to io.Reader

--- fun17/mypackage/blogposts.go
+++ fun17/mypackage/blogposts.go
@@ -46,8 +46,60 @@ func NewPostsFromFS(fileSystem fs.FS) ([]Post, error) {
 		return nil, err
 	}
 	var posts []Post
-	for range dir {
-		posts = append(posts, Post{})
+	for _, f := range dir {
+		post, err := loosenSeparatedGetPost(fileSystem, f)
+		if err != nil {
+			return nil, err // todo: needs clarification, should we totally fail if one file fails? or just ignore?
+		}
+		posts = append(posts, post)
 	}
 	return posts, nil
+}
+
+// This code should be separated from opening file code and from parsing file contents code
+// to make the code simpler to understand and work with
+func getPost(fileSystem fs.FS, f fs.DirEntry) (Post, error) {
+	postFile, err := fileSystem.Open(f.Name())
+	if err != nil {
+		return Post{}, err
+	}
+	defer postFile.Close()
+
+	postData, err := io.ReadAll(postFile)
+	if err != nil {
+		return Post{}, err
+	}
+
+	// for now, we do not need any sophisticated parsing,
+	// just cutting out the `Title:` text by slicing the string.
+	post := Post{Title: string(postData)[7:]}
+	return post, nil
+}
+
+/*
+Think about coupling and cohesion. In this case you should ask youreslef:
+Does newPost have to be coupled to an fs.File? Do we use all the methods and data from this
+type? what do we really need?
+
+In our case we only use it as an argument to io.ReadAll which needs an io.Reader. so we should
+loosen the coupling in our function and ask for an io.Reader.
+
+So we are going to loosen the function newPost by using the io.Reader type
+*/
+func separatedGetPost(fileSystem fs.FS, f fs.DirEntry) (Post, error) {
+	postFile, err := fileSystem.Open(f.Name())
+	if err != nil {
+		return Post{}, err
+	}
+	defer postFile.Close()
+	return newPost(postFile)
+}
+
+func loosenSeparatedGetPost(fileSystem fs.FS, f fs.DirEntry) (Post, error) {
+	postFile, err := fileSystem.Open(f.Name())
+	if err != nil {
+		return Post{}, err
+	}
+	defer postFile.Close()
+	return loosenNewPost(postFile)
 }
