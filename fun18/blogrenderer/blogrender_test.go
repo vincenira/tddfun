d6aa0e7ad3c1929c44cc06700e5acd26c5c57f1a
Raoul NIYO <vincenira@gmail.com>
2025-06-04 18:37:12 -0400

    refactor: initial implementation of the markdown written
    Confirm safety-net of a passing test for working code

--- fun18/blogrenderer/blogrender_test.go
+++ fun18/blogrenderer/blogrender_test.go
@@ -11,7 +11,7 @@ func TestRender(t *testing.T) {
 	aPost := blogrenderer.Post{
 		Title:       "hello world",
 		Body:        "This is a post",
-		Description: "This is description",
+		Description: "This is a description",
 		Tags:        []string{"go", "tdd"},
 	}
 	t.Run("It converts a single post into HTML", func(t *testing.T) {
@@ -22,8 +22,7 @@ func TestRender(t *testing.T) {
 		}
 
 		got := buf.String()
-		want := `<h1>hello world</h1>`
-
+		want := `<h1>hello world</h1><p>This is a description</p>Tags: <ul><li>go</li><li>tdd</li></ul>`
 		if got != want {
 			t.Errorf("got '%s' want '%s'", got, want)
 		}
