aca342709a6b05fcf17361d8217290caa9c31b1b
Raoul NIYO <vincenira@gmail.com>
2025-06-09 22:27:15 -0400

    feat: add new functions in PostRenderer

--- fun18/blogrenderer/blogrender.go
+++ fun18/blogrenderer/blogrender.go
@@ -8,9 +8,31 @@ import (
 
 // if you're continuing from the read files chapte, you shouldn't redefine this
 
+type PostRenderer struct {
+	templ *template.Template
+}
 type Post struct {
 	Title, Description, Body string
 	Tags                     []string
+}
+
+func NewPostRenderer() (*PostRenderer, error) {
+	templ, err := template.ParseFS(postTemplates, "templates/*gohtml")
+	if err != nil {
+		return nil, err
+	}
+	return &PostRenderer{templ: templ}, nil
+}
+
+func (r *PostRenderer) Render(w io.Writer, p Post) error {
+	if err := r.templ.ExecuteTemplate(w, "blog.gohtml", p); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r *PostRenderer) RenderIndex(w io.Writer, posts []Post) error {
+	return nil
 }
 
 //go:embed "templates/*"
