65eb024e2b17409ad4b4906f4bfafdd661d09827
Raoul NIYO <vincenira@gmail.com>
2025-06-10 21:14:52 -0400

    feat: add logic to render the body

--- fun18/blogrenderer/blogrender.go
+++ fun18/blogrenderer/blogrender.go
@@ -4,12 +4,17 @@ import (
 	"embed"
 	"html/template"
 	"io"
+	"strings"
+
+	"github.com/gomarkdown/markdown/parser"
+	"github.com/hashicorp/hcl/json/parser"
 )
 
 // if you're continuing from the read files chapte, you shouldn't redefine this
 
 type PostRenderer struct {
-	templ *template.Template
+	templ    *template.Template
+	mdParser *parser.Parser
 }
 type Post struct {
 	Title, Description, Body string
@@ -29,11 +34,14 @@ func NewPostRenderer() (*PostRenderer, error) {
 	if err != nil {
 		return nil, err
 	}
-	return &PostRenderer{templ: templ}, nil
+
+	extensions := parser.CommonExtensions | parser.AutoHeadingIDs
+	parser := parser.NewWithExtensions(extensions)
+	return &PostRenderer{templ: templ, mdParser: parser}, nil
 }
 
 func (r *PostRenderer) Render(w io.Writer, p Post) error {
-	if err := r.templ.ExecuteTemplate(w, "blog.gohtml", p); err != nil {
+	if err := r.templ.ExecuteTemplate(w, "blog.gohtml", newPostVM(p, r)); err != nil {
 		return err
 	}
 	return nil
@@ -65,4 +73,15 @@ func Render(w io.Writer, p Post) error {
 		return err
 	}
 	return nil
+}
+
+type postViewModel struct {
+	Post
+	HTMLBody template.HTML
+}
+
+func newPostVM(p Post, r *PostRenderer) postViewModel {
+	vm := postViewModel{Post: p}
+	vm.HTMLBody = template.HTML(markdown.ToHTML([]byte(p.Body), r.mdParser, nil))
+	return vm
 }
