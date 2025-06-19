115a770b4cd8f18595f7ce720fbe2da612cf613d
Raoul NIYO <vincenira@gmail.com>
2025-06-18 21:41:06 -0400

    test: add testcase for interface{}

--- fun19/generics/asserts_test.go
+++ fun19/generics/asserts_test.go
@@ -64,6 +64,13 @@ func AssertEqual[T comparable](t *testing.T, got, want T) {
 	}
 }
 
+func AssertEqualInterface(t *testing.T, got, want interface{}) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %v, want %v", got, want)
+	}
+}
+
 func AssertNotEqual[T comparable](t *testing.T, got, want T) {
 	t.Helper()
 	if got == want {
