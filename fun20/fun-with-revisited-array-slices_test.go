98cdf928803066d40d5689728baf9c283f9bc5ed
Raoul NIYO <vincenira@gmail.com>
2025-06-21 16:01:53 -0400

    test: add the initial testcase for the reduce pattern

---
+++ fun20/fun-with-revisited-array-slices_test.go
@@ -0,0 +1,12 @@
+package fun20
+
+import "testing"
+
+func TestReduce(t *testing.T) {
+	t.Run("multiplication of all elements", func(t *testing.T) {
+		multiply := func(x, y int) int {
+			return x * y
+		}
+		AssertEqual(t, Reduce([]int{1, 2, 3}, multiply, 1), 6)
+	})
+}
