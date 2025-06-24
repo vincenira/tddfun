b990eb9c4de2ff322adaaa1275f29ea33c01ad16
Raoul NIYO <vincenira@gmail.com>
2025-06-23 21:42:15 -0400

    feat: add the functions and data types for the transactions
    write enough code to make it pass

---
+++ fun20/fun-with-transactions.go
@@ -0,0 +1,20 @@
+package fun20
+
+type Transaction struct {
+	From string
+	To   string
+	Sum  float64
+}
+
+func BalanceFor(transactions []Transaction, name string) float64 {
+	var balance float64
+	for _, t := range transactions {
+		if t.From == name {
+			balance -= t.Sum
+		}
+		if t.To == name {
+			balance += t.Sum
+		}
+	}
+	return balance
+}
