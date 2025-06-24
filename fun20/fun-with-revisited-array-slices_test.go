5bb57d7f7e9efc1b32b95a73d5229b371de070ad
Raoul NIYO <vincenira@gmail.com>
2025-06-23 21:23:28 -0400

    test: add a new testcase for strings

--- fun20/fun-with-revisited-array-slices_test.go
+++ fun20/fun-with-revisited-array-slices_test.go
@@ -11,4 +11,10 @@ func TestReduce(t *testing.T) {
 		}
 		AssertEqual(t, Reduce([]int{1, 2, 3}, multiply, 1), 6)
 	})
+	t.Run("concatenate strings", func(t *testing.T) {
+		concatenate := func(x, y string) string {
+			return x + y
+		}
+		AssertEqual(t, Reduce([]string{"a", "b", "c"}, concatenate, ""), "abc")
+	})
 }
