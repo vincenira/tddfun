14203bb679cf0fd9f7ebd26e75836e79983b9d48
Raoul NIYO <vincenira@gmail.com>
2025-07-03 23:11:31 -0400

    refactor: add a newbalancefor function

--- fun20/fun-with-transactions.go
+++ fun20/fun-with-transactions.go
@@ -15,6 +15,9 @@ func NewTransaction(from, to Account, sum float64) Transaction {
 	return Transaction{From: from.Name, To: to.Name, Sum: sum}
 }
 
+func NewBalanceFor(account Account, transactions []Transaction) Account {
+	return Reduce(transactions, applyTransaction, account)
+}
 func BalanceFor(transactions []Transaction, name string) float64 {
 	adjustBalance := func(currentBalance float64, t Transaction) float64 {
 		if t.From == name {
